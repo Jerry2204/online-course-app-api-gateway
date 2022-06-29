bbdbe254713b67f83280650c1321f23207e3dc86
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-29 09:49:05 +0700

    integration post media to api gateway

--- routes/media.js
+++ routes/media.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
-const { APP_NAME } = process.env;
+const express = require('express');
+const router = express.Router();
+
+const mediaHandler = require('./handler/media');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('media');
+router.get('/', (req, res) => {
+  return res.json({ message: process.env.URL_SERVICE_MEDIA });
 });
+router.post('/', mediaHandler.create);
 
 module.exports = router;
