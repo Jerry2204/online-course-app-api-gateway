7a05aefd1ea613afba4f46fbeb6d602d96d7ec37
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-06-29 09:56:29 +0700

    integration get all media to api gateway

--- routes/media.js
+++ routes/media.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const mediaHandler = require('./handler/media');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  return res.json({ message: process.env.URL_SERVICE_MEDIA });
-});
+router.get('/', mediaHandler.getAll);
 router.post('/', mediaHandler.create);
 
 module.exports = router;
